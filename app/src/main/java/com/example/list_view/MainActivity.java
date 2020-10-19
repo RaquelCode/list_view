f3e744833f30e3d5e4952f23fa004508b2235b9f
Raquel <rqlmoura@yahoo.com>
2020-10-19 15:46:54 -0700

    Add a new layout called "listview_layout

--- app/src/main/java/com/example/list_view/MainActivity.java
+++ app/src/main/java/com/example/list_view/MainActivity.java
@@ -10,5 +10,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] cities = {"Seattle",
+                "Bothell",
+                "Kirkland",
+                "Bellevue",
+                "Lynnwood",
+                "Renton",
+                "Redmond",
+                "Spokane",
+                "Vancouver",
+                "Tacoma",
+                "Olympia",
+                "Bellingham",
+                "Arlington",
+                "Everett",
+                "Woodinville",
+                "Monroe",
+                "New Castle",
+                "Ballard",
+                "Lacey"};
     }
 }
