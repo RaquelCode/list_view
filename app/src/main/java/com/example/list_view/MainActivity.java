d01919794cc7475e23c4f3eb2c280c15a6ed1e29
Raquel <rqlmoura@yahoo.com>
2020-10-19 20:44:28 -0700

    Add a TextView with id "city" to the new layout Create the ArrayAdapter with this data

--- app/src/main/java/com/example/list_view/MainActivity.java
+++ app/src/main/java/com/example/list_view/MainActivity.java
@@ -3,32 +3,38 @@ package com.example.list_view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+   private final  String[] cities = {"Seattle",
+            "Bothell",
+            "Kirkland",
+            "Bellevue",
+            "Lynnwood",
+            "Renton",
+            "Redmond",
+            "Spokane",
+            "Vancouver",
+            "Tacoma",
+            "Olympia",
+            "Bellingham",
+            "Arlington",
+            "Everett",
+            "Woodinville",
+            "Monroe",
+            "New Castle",
+            "Ballard",
+            "Lacey"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] cities = {"Seattle",
-                "Bothell",
-                "Kirkland",
-                "Bellevue",
-                "Lynnwood",
-                "Renton",
-                "Redmond",
-                "Spokane",
-                "Vancouver",
-                "Tacoma",
-                "Olympia",
-                "Bellingham",
-                "Arlington",
-                "Everett",
-                "Woodinville",
-                "Monroe",
-                "New Castle",
-                "Ballard",
-                "Lacey"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.listview_layout,R.id.city,cities);
+       ListView lv = findViewById( R.id.myCities);
+       lv.setAdapter(adapter);
     }
 }
